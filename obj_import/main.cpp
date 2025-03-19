23a70ac173a545b9b6ca9269219d253db2961d06
Краснов Егор <flowegor@mail.ru>
2025-03-19 11:32:45 +0700

    Добавьте файлы проекта.

---
+++ obj_import/main.cpp
@@ -0,0 +1,172 @@
+#include <glad.h>
+#include <glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader_m.h"
+#include "camera.h"
+#include "model.h"
+
+
+#include <iostream>
+#include "stb_image.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+// Константы
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Камера
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// Тайминги
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main()
+{
+   // glfw: инициализация и конфигурирование
+   glfwInit();
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+   // glfw: создание окна
+   GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com!", NULL, NULL);
+   if (window == NULL)
+   {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return -1;
+   }
+   glfwMakeContextCurrent(window);
+   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+   glfwSetCursorPosCallback(window, mouse_callback);
+   glfwSetScrollCallback(window, scroll_callback);
+
+   // Сообщаем GLFW, чтобы он захватил наш курсор
+   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+   // glad: загрузка всех указателей на OpenGL-функции
+   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+   {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+   }
+
+   // Сообщаем stb_image.h, чтобы он перевернул загруженные текстуры относительно y-оси (до загрузки модели)
+   //stbi_set_flip_vertically_on_load(true);
+
+   // Конфигурирование глобального состояния OpenGL
+   glEnable(GL_DEPTH_TEST);
+
+   // Компилирование нашей шейдерной программы
+   Shader ourShader("C:/Users/flowe/source/repos/obj_import/obj_import/1.model_loading.vs", "C:/Users/flowe/source/repos/obj_import/obj_import/1.model_loading.fs");
+
+   // Загрузка моделей
+   //Model ourModel("C:/Users/flowe/source/repos/obj_import/obj_import/resources/objects/backpack/backpack.obj");
+   Model ourModel("C:/Users/flowe/source/repos/obj_import/obj_import/resources/objects/cat/12221_Cat_v1_l3.obj");
+   //Model ourModel("C:/Users/flowe/source/repos/obj_import/obj_import/resources/objects/skull/12140_Skull_v3_L2.obj");
+
+   // Отрисовка в режиме каркаса
+   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+   // Цикл рендеринга
+   while (!glfwWindowShouldClose(window))
+   {
+      // Логическая часть работы со временем для каждого кадра
+      float currentFrame = glfwGetTime();
+      deltaTime = currentFrame - lastFrame;
+      lastFrame = currentFrame;
+
+      // Обработка ввода
+      processInput(window);
+
+      // Рендеринг
+      glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      // Убеждаемся, что активировали шейдер прежде, чем настраивать uniform-переменные/объекты_рисования
+      ourShader.use();
+
+      // Преобразования Вида/Проекции
+      glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+      glm::mat4 view = camera.GetViewMatrix();
+      ourShader.setMat4("projection", projection);
+      ourShader.setMat4("view", view);
+
+      // Рендеринг загруженной модели
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // смещаем вниз чтобы быть в центре сцены
+      model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// объект слишком большой для нашей сцены, поэтому немного уменьшим его
+      ourShader.setMat4("model", model);
+      ourModel.Draw(ourShader);
+
+
+      // glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+   }
+
+   // glfw: завершение, освобождение всех выделенных ранее GLFW-реcурсов
+   glfwTerminate();
+   return 0;
+}
+
+// Обработка всех событий ввода: запрос GLFW о нажатии/отпускании кнопки мыши в данном кадре и соответствующая обработка данных событий
+void processInput(GLFWwindow* window)
+{
+   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
+
+   if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      camera.ProcessKeyboard(FORWARD, deltaTime);
+   if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      camera.ProcessKeyboard(BACKWARD, deltaTime);
+   if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+      camera.ProcessKeyboard(LEFT, deltaTime);
+   if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+      camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+   // Убеждаемся, что окно просмотра соответствует новым размерам окна.
+   // Обратите внимание, ширина и высота будут значительно больше, чем указано, на Retina-дисплеях
+   glViewport(0, 0, width, height);
+}
+
+// glfw: всякий раз, когда перемещается мышь, вызывается данная callback-функция
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+   if (firstMouse)
+   {
+      lastX = xpos;
+      lastY = ypos;
+      firstMouse = false;
+   }
+
+   float xoffset = xpos - lastX;
+   float yoffset = lastY - ypos; // перевернуто, так как y-координаты идут снизу вверх
+
+   lastX = xpos;
+   lastY = ypos;
+
+   camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: всякий раз, когда прокручивается колесико мыши, вызывается данная callback-функция
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+   camera.ProcessMouseScroll(yoffset);
+}
